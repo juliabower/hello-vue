2f9bbe836496ea21921e42ff135bfe4edba93ac0
Julia Bower <julia.bower@sparkventures.co.nz>
2017-08-02 07:50:53 +1200

    first commit, hello world vue project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <img src="./assets/unicorn.gif" class="unicorn">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss" src="./styles.scss"></style>
