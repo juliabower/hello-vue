44421ad179a1cb29345517131bd473705f301a7f
Julia Bower <julia.bower@sparkventures.co.nz>
2017-08-23 08:52:49 +1200

    messing around with hello unit tests

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,22 +1,40 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import Hello from '@/components/Hello'
+// import Vuex from 'vuex'
+import Hello from '../../../src/components/Hello/Hello.vue'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
+//
+// describe('Hello', () => {
+//  let store
+//  beforeEach(() => {
+//    store = new Vuex.Store({
+//      state: {
+//        message: 'This is coming from the vuex store'
+//      }
+//    })
+//  })
+//  it('should render correct contents', () => {
+//    const Constructor = Vue.extend(Hello)
+//    const vm = new Constructor().$mount(null, {store})
+//    expect(vm.$el.querySelector('.hello h1').textContent)
+//      .to.equal('Hello World!')
+//  })
+// })
 
-describe('Hello', () => {
-  let store
-  beforeEach(() => {
-    store = new Vuex.Store({
-      state: {
-        message: 'This is coming from the vuex store'
-      }
-    })
+describe('Hello.vue', () => {
+  it('has a created hook', () => {
+    expect(Hello.created).to.be.a('function')
   })
-  it('should render correct contents', () => {
+
+  it('sets the correct default data', () => {
+    expect(Hello.data).to.be.a('function')
+    const defaultData = Hello.data()
+    expect(defaultData.msg).to.equal('Hello World!')
+  })
+
+  it('renders the correct message', () => {
     const Constructor = Vue.extend(Hello)
-    const vm = new Constructor().$mount(null, {store})
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Hello World!')
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.title').textContent).to.equal('Hello World!')
   })
 })
